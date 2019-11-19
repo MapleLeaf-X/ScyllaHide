ea03f187c8967a4078938df534df77ecbd10617a
MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>
2019-11-19 04:38:13 +0200

    Shim changes

--- Scylla/NtApiShim.h
+++ Scylla/NtApiShim.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef NTAPISHI_H
+#define NTAPISHI_H
 
 #include <ntdll/ntdll.h>
 
@@ -23,81 +24,82 @@
 #endif
 
 #ifndef DBG_PRINTEXCEPTION_WIDE_C
-#define DBG_PRINTEXCEPTION_WIDE_C ((DWORD)0x4001000A)
+#define DBG_PRINTEXCEPTION_WIDE_C (0x4001000A)
 #endif
 
-typedef NTSTATUS(WINAPI *t_NtWow64QueryInformationProcess64)(HANDLE ProcessHandle, PROCESSINFOCLASS ProcessInformationClass, PVOID ProcessInformation, ULONG ProcessInformationLength, PULONG ReturnLength);
-typedef NTSTATUS(WINAPI *t_NtWow64ReadVirtualMemory64)(HANDLE ProcessHandle, PVOID64 BaseAddress, PVOID Buffer, ULONGLONG BufferSize, PULONGLONG NumberOfBytesRead);
-typedef NTSTATUS(WINAPI *t_NtWow64WriteVirtualMemory64)(HANDLE ProcessHandle, PVOID64 BaseAddress, LPCVOID Buffer, ULONGLONG BufferSize, PULONGLONG NumberOfBytesWritten);
+typedef NTSTATUS (NTAPI * NtWow64QueryInformationProcess64_t)(HANDLE ProcessHandle,
+	PROCESSINFOCLASS ProcessInformationClass, PVOID ProcessInformation,
+	ULONG ProcessInformationLength, PULONG ReturnLength);
+typedef NTSTATUS (NTAPI * NtWow64ReadVirtualMemory64_t)(HANDLE ProcessHandle,
+	PVOID64 BaseAddress, PVOID Buffer, ULONGLONG BufferSize, PULONGLONG NumberOfBytesRead);
+typedef NTSTATUS (NTAPI * NtWow64WriteVirtualMemory64_t)(HANDLE ProcessHandle,
+	PVOID64 BaseAddress, LPCVOID Buffer, ULONGLONG BufferSize, PULONGLONG NumberOfBytesWritten);
 
 
-namespace scl
-{
-    template <typename PTR>
-    struct UNICODE_STRING
-    {
-        union
-        {
-            struct
-            {
-                WORD Length;
-                WORD MaximumLength;
-            };
-            PTR dummy;
-        };
-        PTR _Buffer;
-    };
+namespace scl {
+	template <typename PTR>
+	struct UNICODE_STRING {
+		union {
+			struct {
+				WORD Length;
+				WORD MaximumLength;
+			};
+			PTR dummy;
+		};
+		PTR _Buffer;
+	};
 
-    template<typename PTR>
-    struct CURDIR {
-        UNICODE_STRING<PTR> DosPath;
-        PTR Handle;
-    };
+	template<typename PTR>
+	struct CURDIR {
+		UNICODE_STRING<PTR> DosPath;
+		PTR Handle;
+	};
 
-    template<typename PTR>
-    struct PROCESS_BASIC_INFORMATION
-    {
-        DWORD ExitStatus;
-        PTR PebBaseAddress;
-        PTR AffinityMask;
-        DWORD BasePriority;
-        PTR UniqueProcessId;
-        PTR InheritedFromUniqueProcessId;
-    };
+	template<typename PTR>
+	struct PROCESS_BASIC_INFORMATION {
+		DWORD ExitStatus;
+		PTR PebBaseAddress;
+		PTR AffinityMask;
+		DWORD BasePriority;
+		PTR UniqueProcessId;
+		PTR InheritedFromUniqueProcessId;
+	};
 
-    template<typename PTR>
-    struct RTL_USER_PROCESS_PARAMETERS {
-        ULONG MaximumLength;
-        ULONG Length;
+	template<typename PTR>
+	struct RTL_USER_PROCESS_PARAMETERS {
+		ULONG MaximumLength;
+		ULONG Length;
 
-        ULONG Flags;
-        ULONG DebugFlags;
+		ULONG Flags;
+		ULONG DebugFlags;
 
-        PTR ConsoleHandle;
-        ULONG  ConsoleFlags;
-        PTR StandardInput;
-        PTR StandardOutput;
-        PTR StandardError;
+		PTR ConsoleHandle;
+		ULONG  ConsoleFlags;
+		PTR StandardInput;
+		PTR StandardOutput;
+		PTR StandardError;
 
-        CURDIR<PTR> CurrentDirectory;
-        UNICODE_STRING<PTR> DllPath;
-        UNICODE_STRING<PTR> ImagePathName;
-        UNICODE_STRING<PTR> CommandLine;
-        PTR Environment;
+		CURDIR<PTR> CurrentDirectory;
+		UNICODE_STRING<PTR> DllPath;
+		UNICODE_STRING<PTR> ImagePathName;
+		UNICODE_STRING<PTR> CommandLine;
+		PTR Environment;
 
-        ULONG StartingX;
-        ULONG StartingY;
-        ULONG CountX;
-        ULONG CountY;
-        ULONG CountCharsX;
-        ULONG CountCharsY;
-        ULONG FillAttribute;
+		ULONG StartingX;
+		ULONG StartingY;
+		ULONG CountX;
+		ULONG CountY;
+		ULONG CountCharsX;
+		ULONG CountCharsY;
+		ULONG FillAttribute;
 
-        ULONG WindowFlags;
-        ULONG ShowWindowFlags;
-        UNICODE_STRING<PTR> WindowTitle;
-        UNICODE_STRING<PTR> DesktopInfo;
-        UNICODE_STRING<PTR> ShellInfo;
-        UNICODE_STRING<PTR> RuntimeData;
-    };
+		ULONG WindowFlags;
+		ULONG ShowWindowFlags;
+		UNICODE_STRING<PTR> WindowTitle;
+		UNICODE_STRING<PTR> DesktopInfo;
+		UNICODE_STRING<PTR> ShellInfo;
+		UNICODE_STRING<PTR> RuntimeData;
+	};
 }
+
+#endif //NTAPISHI_H
