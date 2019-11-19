e147316e609fbcc7ea93bb480511893e986b2f91
MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>
2019-11-20 00:06:40 +0200

    Signed-off-by: MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>

--- Scylla/Resource.h
+++ Scylla/Resource.h
@@ -1,25 +1,25 @@
-#pragma once
+#ifndef RESOURCE_H
+#define RESOURCE_H
 
-#include <Windows.h>
+#include <windows.h>
 
-namespace scl
-{
-    class Handle
-    {
-    public:
-        explicit Handle(HANDLE handle) : handle_(handle) {}
-        ~Handle()
-        {
-            if (handle_ && (handle_ != INVALID_HANDLE_VALUE))
-                CloseHandle(handle_);
-        }
+namespace scl {
+	class Handle {
+	public:
+		explicit Handle(HANDLE handle) : handle_(handle) {}
+		~Handle() {
+			if(handle_ && (handle_ != INVALID_HANDLE_VALUE))
+				CloseHandle(handle_);
+		}
 
-        Handle(const Handle &other) = delete;
-        Handle &operator=(const Handle &other) = delete;
+		Handle(const Handle &other) = delete;
+		Handle &operator=(const Handle &other) = delete;
 
-        HANDLE get() const { return handle_; }
+		HANDLE get() const { return handle_; }
 
-    private:
-        HANDLE handle_;
-    };
+	private:
+		HANDLE handle_;
+	};
 }
+
+#endif //RESOURCE_H
