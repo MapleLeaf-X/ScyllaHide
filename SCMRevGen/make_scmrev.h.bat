e0d5e915553a81e5cdf5b2dd2c5ea105ecde7f77
MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>
2020-09-06 17:26:50 +0300

    Versioning generation update

--- SCMRevGen/make_scmrev.h.bat
+++ SCMRevGen/make_scmrev.h.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set GIT_VERSION_NUMBER=0 
+set GIT_VERSION_NUMBER=0
 set GIT_SHORT_HASH=unknown
 
 where git 1>nul 2>&1
@@ -29,9 +29,24 @@ echo.>> scmrev.h
 echo #define GIT_VERSION_NUMBER %GIT_VERSION_NUMBER%>> scmrev.h
 echo #define GIT_SHORT_HASH_A "%GIT_SHORT_HASH%">> scmrev.h
 echo #define GIT_SHORT_HASH_W L"%GIT_SHORT_HASH%">> scmrev.h
+echo #ifdef UNICODE>> scmrev.h
+echo #define GIT_SHORT_HASH  GIT_SHORT_HASH_W>> scmrev.h
+echo #else>> scmrev.h
+echo #define GIT_SHORT_HASH  GIT_SHORT_HASH_A>> scmrev.h
+echo #endif>> scmrev.h
 echo #define COMPILE_DATE_A "%COMPILE_DATE%">> scmrev.h
 echo #define COMPILE_DATE_W L"%COMPILE_DATE%">> scmrev.h
+echo #ifdef UNICODE>> scmrev.h
+echo #define COMPILE_DATE  COMPILE_DATE_W>> scmrev.h
+echo #else>> scmrev.h
+echo #define COMPILE_DATE  COMPILE_DATE_A>> scmrev.h
+echo #endif>> scmrev.h
 echo #define COMPILE_YEAR_A "%COMPILE_YEAR%">> scmrev.h
 echo #define COMPILE_YEAR_W L"%COMPILE_YEAR%">> scmrev.h
+echo #ifdef UNICODE>> scmrev.h
+echo #define COMPILE_YEAR  COMPILE_YEAR_W>> scmrev.h
+echo #else>> scmrev.h
+echo #define COMPILE_YEAR  COMPILE_YEAR_A>> scmrev.h
+echo #endif>> scmrev.h
 echo.>> scmrev.h
 echo #endif //SCMREV_H>> scmrev.h
--- Scylla/Version.cpp
+++ Scylla/Version.cpp
@@ -1,10 +1,11 @@
 #include "Version.h"
 
 void scl::ShowAboutBox(HWND hWnd) {
-	::MessageBoxA(hWnd,
-		SCYLLA_HIDE_NAME_A " Plugin v" SCYLLA_HIDE_VERSION_STRING_A " (" TEXT(__DATE__) ")\n\n"
-		"Copyright (C) 2014-" COMPILE_YEAR_A " Aguila / cypher\n\n"
-		"Special thanks to:\n"
+	MessageBox(hWnd,
+		SCYLLA_HIDE_NAME STR(" Plugin v") SCYLLA_HIDE_VERSION_STRING
+		STR(" (") TEXT(__DATE__) STR(")\n\n")
+		STR("Copyright (C) 2014-") COMPILE_YEAR STR(" Aguila / cypher\n\n")
+		STR("Special thanks to:\n"
 		"- What for his POISON assembler source code\n"
 		"- waliedassar for his blog posts\n"
 		"- Peter Ferrie for his Anti-Debug PDFs\n"
@@ -12,6 +13,6 @@ void scl::ShowAboutBox(HWND hWnd) {
 		"- Steve Micallef for his IDA SDK doc\n"
 		"- Authors of PhantOm and StrongOD\n"
 		"- Tuts4You, Exetools, Exelab community for testing\n"
-		"- last but not least deepzero & mr.exodia for tech chats",
-		SCYLLA_HIDE_NAME_A " Plugin", MB_ICONINFORMATION);
+		"- last but not least deepzero & mr.exodia for tech chats"),
+		SCYLLA_HIDE_NAME STR(" Plugin"), MB_ICONINFORMATION);
 }
