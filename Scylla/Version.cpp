c44aaff541c3908148eb5cfc13d7cf1f0ec52fdb
MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>
2020-09-07 13:28:22 +0300

    Messagebox fix

--- Scylla/Version.cpp
+++ Scylla/Version.cpp
@@ -2,17 +2,17 @@
 
 void scl::ShowAboutBox(HWND hWnd) {
 	MessageBox(hWnd,
-		SCYLLA_HIDE_NAME STR(" Plugin v") SCYLLA_HIDE_VERSION_STRING
-		STR(" (") TEXT(__DATE__) STR(")\n\n")
-		STR("Copyright (C) 2014-") COMPILE_YEAR STR(" Aguila / cypher\n\n")
-		STR("Special thanks to:\n"
-		"- What for his POISON assembler source code\n"
-		"- waliedassar for his blog posts\n"
-		"- Peter Ferrie for his Anti-Debug PDFs\n"
-		"- MaRKuS-DJM for OllyAdvanced assembler source code\n"
-		"- Steve Micallef for his IDA SDK doc\n"
-		"- Authors of PhantOm and StrongOD\n"
-		"- Tuts4You, Exetools, Exelab community for testing\n"
-		"- last but not least deepzero & mr.exodia for tech chats"),
-		SCYLLA_HIDE_NAME STR(" Plugin"), MB_ICONINFORMATION);
+		SCYLLA_HIDE_NAME TEXT(" Plugin v") SCYLLA_HIDE_VERSION_STRING
+		TEXT(" (") TEXT(__DATE__) TEXT(")\n\n")
+		TEXT("Copyright (C) 2014-") COMPILE_YEAR TEXT(" Aguila / cypher\n\n")
+		TEXT("Special thanks to:\n")
+		TEXT("- What for his POISON assembler source code\n")
+		TEXT("- waliedassar for his blog posts\n")
+		TEXT("- Peter Ferrie for his Anti-Debug PDFs\n")
+		TEXT("- MaRKuS-DJM for OllyAdvanced assembler source code\n")
+		TEXT("- Steve Micallef for his IDA SDK doc\n")
+		TEXT("- Authors of PhantOm and StrongOD\n")
+		TEXT("- Tuts4You, Exetools, Exelab community for testing\n")
+		TEXT("- last but not least deepzero & mr.exodia for tech chats"),
+		SCYLLA_HIDE_NAME TEXT(" Plugin"), MB_ICONINFORMATION);
 }
